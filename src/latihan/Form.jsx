63c686a5e2c5fe70e3459ff36599a6a00f7ccf00
kmilawn <najwahk354@gmail.com>
2026-06-09 08:54:00 +0700

    Latihan - Post Data

--- src/latihan/Form.jsx
+++ src/latihan/Form.jsx
@@ -1,23 +1,48 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Form = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(email);
-        console.log(password);
-        console.log("-");
+
+        try {
+            const response = await axios.post(
+                "https://jwt-auth-eight-neon.vercel.app/login",
+                { email, password },
+            );
+
+            console.log(response.data);
+            localStorage.setItem("token", response.data.refreshToken);
+            } catch (error) {
+            console.error(error);
+        }
     };
 
   return (
     <div className="p-4">
       <form onSubmit={handleSubmit}>
-        Email :<input type="text" id="email" name="email" className="border" value={email} onChange={(e) => setEmail(e.target.value)}  />
+        Email :
+        <input 
+            type="text" 
+            id="email" 
+            name="email" 
+            className="border" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}  
+        />
         <br />
         Password :
-        <input type="text" id="password" name="password" className="border" value={password} onChange={(e) => setPassword(e.target.value)}  />
+        <input 
+            type="text" 
+            id="password" 
+            name="password" 
+            className="border" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)}  
+        />
         <br />
         <input type="submit" value="send" className="bg-gray-200 p-2" />
       </form>
